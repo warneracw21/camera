ea72802becaebd06eddcc965f9f8c9ff8b80ff12
warneracw21 <warneracw@gmail.com>
2019-04-11 22:19:01 +0100

    set threading

--- button.py
+++ button.py
@@ -13,18 +13,27 @@ GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 camera = PiCamera()
 
 def take_picture():
-	global i
-	i += 1
-	image_name = os.path.join(config.ROOT, 'pic_%d.jpg' % i)
 
+	# Referencing Global Variable (shared across threads)
+	global image_num
+	image_num += 1
+
+	# Build Image Write Path
+	image_name = os.path.join(config.ROOT, 'pic_%d.jpg' % image_num)
+
+	# Set Lighting (takes 3 seconds)
 	time.sleep(3)
 	camera.capture(image_name)
 	print("Image Taken:", image_name)
 
+# Define Callback lambda and assign event handler
+callback = lambda x: take_picture()
+GPIO.add_event_detect(config.CHANNEL, 
+					  GPIO.RISING, 
+					  callback=callback, 
+					  bouncetime=200)
 
-GPIO.add_event_detect(config.CHANNEL, GPIO.RISING, callback=lambda x: take_picture(), bouncetime=200)
-
-i = 0
+image_num = 0
 while True:
 	GPIO.event_detected(config.CHANNEL)
 
