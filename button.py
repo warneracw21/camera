ddb988ff58ab5d98a899509c7f2d0c38c208b2b9
warneracw21 <warneracw@gmail.com>
2019-04-12 04:26:41 +0100

    changes

--- button.py
+++ button.py
@@ -1,10 +1,17 @@
-from picamera import PiCamera
-import RPi.GPIO as GPIO
-import time
+from __future__ import print_function
 
 import os
+import sys
 
-from camera import take_picture
+from picamera import PiCamera
+from zipfile import ZipFile
+from send_package import send_zip
+import RPi.GPIO as GPIO
+
+import time
+import math
+import glob
+
 import config
 
 GPIO.setmode(GPIO.BCM)
@@ -12,28 +19,54 @@ GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 camera = PiCamera()
 
-def take_picture():
+fall = math.inf
+def take_picture(channel):
 
-	# Referencing Global Variable (shared across threads)
-	global image_num
-	image_num += 1
+	global fall, image_num
 
-	# Build Image Write Path
-	image_name = os.path.join(config.ROOT, 'pic_%d.jpg' % image_num)
+	if not(GPIO.input(channel)):
+		fall = time.time()
 
-	# Set Lighting (takes 3 seconds)
-	time.sleep(3)
-	camera.capture(image_name)
-	print("Image Taken:", image_name)
+	else:
+		if(math.fabs(time.time() - fall) > 1.0):
+			print("Zipping File ...", end=" ", flush=True)
+			files = list(glob.glob(os.path.join(config.ROOT, '*')))
+			if files:
+				with ZipFile(config.ZIP_NAME, 'w') as zp:
+					for f in files:
+						zp.write(os.path.relpath(f))
+			print("Success")
+			print("Sending Zip ...", end=" ", flush=True)
+			response = send_zip()
+			if response.ok:
+				print("Success")
+				for f in files:
+					os.remove(f)
+				os.remove(os.path.join(os.getcwd(), config.ZIP_NAME))
+			else:
+				print(response)
 
+
+		else:
+			
+			image_path = os.path.join(config.ROOT, 'pic_%d.jpg' % image_num)
+			print("Taking Photo ... ", end=" ", flush=True)
+			time.sleep(3)
+			camera.capture(image_path)
+			print("Success [%s]" % image_path)
+			image_num += 1
+
+
+	
 # Define Callback lambda and assign event handler
-callback = lambda x: take_picture()
 GPIO.add_event_detect(config.CHANNEL, 
 					  GPIO.RISING, 
-					  callback=callback, 
-					  bouncetime=200)
+					  callback=take_picture, 
+					  bouncetime=100)
 
 image_num = 0
 while True:
+
 	GPIO.event_detected(config.CHANNEL)
+
 
